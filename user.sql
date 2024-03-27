adb4b4968c40d2a4731a4d047c20fa801646d8d5
gwennbrln <bgwenn33@gmail.com>
2024-03-27 11:00:38 +0100

    gout

--- user.sql
+++ user.sql
@@ -10,4 +10,25 @@ CREATE TABLE Utilisateurs (
     Pseudonyme VARCHAR(50) NOT NULL UNIQUE, 
     Email VARCHAR(100) NOT NULL UNIQUE, 
     MotDePasse VARCHAR(255) NOT NULL 
-) ENGINE=InnoDB DEFAULT CHARSET=utf8; 
+) 
+
+CREATE TABLE Gouts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    UtilisateurId INT,
+    FOREIGN KEY (UtilisateurId) REFERENCES Utilisateurs(id),
+    JeuxVideo VARCHAR(100),
+    Sports VARCHAR(100),
+    Hobbies VARCHAR(100),
+    MusiquePreferee VARCHAR(100),
+    FilmsPrefers VARCHAR(100),
+    LivresPrefers VARCHAR(100),
+    RegimeAlimentaire VARCHAR(50),
+    HabitudesDeVie VARCHAR(100),
+    DomainesInteret VARCHAR(100),
+    ValeursPersonnelles VARCHAR(100),
+    OpinionsPolitiques VARCHAR(100),
+    ActivitesSociales VARCHAR(100),
+    ObjectifsAspirations VARCHAR(255),
+);
+
+
