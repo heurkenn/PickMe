c07f65bd907eb6a2e69907514a04a04c64c47f90
heurkenn <heurkenn@gmail.com>
2024-03-29 22:30:23 +0100

    Merci pour le CSS et  gwenn ban

--- InsCon.php
+++ InsCon.php
@@ -4,13 +4,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon Site Web</title>
+    <title>PICK ME !</title>
     <link rel="stylesheet" href="stylesheet.css">
 </head>
 
 <body>
     <header>
-        <h1>Click me!</h1>
+        <h1>PICK ME !</h1>
         <nav>
             <ul>
                 <li><a href="#">Accueil</a></li>
@@ -19,29 +19,27 @@
                 <li><a href="#">Contact</a></li>
             </ul>
         </nav>
+
     </header>
     <div class="main">
         <section class="forms">
             <!-- Formulaire d'inscription -->
             <div id="inscriptionForm" class="hidden">
                 <h2>Formulaire d'inscription</h2>
-                <form method="post" action="inscription.php">
-                    <label for="Nom">Nom :</label><br>
-                    <input type="text" id="Nom" name="Nom"><br>
-                    <label for="Prenom">Prenom :</label><br>
+                <form method="post" action="inscription.php"> <!-- Notez le changement d'action ici -->
+                    <label for="Nom">Nom :</label><br><br>
+                    <input type="text" id="Nom" name="Nom" required><br>
+                    <label for="Prenom">Prenom :</label><br><br>
                     <input type="text" id="Prenom" name="Prenom" required><br>
-                    <label for="DateNaissance">Date de naissance :</label><br>
+                    <label for="DateNaissance">Date de naissance :</label><br><br>
                     <input type="date" id="DateNaissance" name="DateNaissance"><br>
                     <!-- Changé pour correspondre au nom de colonne SQL -->
-                    <label for="Pseudonyme">Pseudonyme :</label><br>
+                    <label for="Pseudonyme">Pseudonyme :</label><br><br>
                     <input type="text" id="Pseudonyme" name="Pseudonyme" required><br>
-                    <label for="Email">Email :</label><br>
+                    <label for="Email">Email :</label><br><br>
                     <input type="email" id="Email" name="Email" required><br>
-                    <label for="MotdePasse">Mot de passe :</label><br> <!-- Correction du label et du name -->
-                    <input type="password" id="MotDePasse" name="MotDePasse"
-                        pattern="(?=.*[A-Z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}"
-                        title="Le mot de passe doit contenir au moins 8 caractères, une majuscule, un chiffre et un caractère spécial."
-                        placeholder="8 caractères, une majuscule, un chiffre et un caractère spécial." required><br>
+                    <label for="Code">Mot de passe :</label><br><br>
+                    <input type="password" id="Code" name="Code" required><br>
                     <input type="submit" value="Envoyer">
                 </form>
             </div>
@@ -49,10 +47,10 @@
             <div id="connexionForm" class="hidden">
                 <h2>Formulaire de connexion</h2>
                 <form method="post" action="connexion.php"> <!-- Notez le changement d'action ici -->
-                    <label for="PseudonymeConnexion">Pseudonyme :</label><br>
+                    <label for="PseudonymeConnexion">Pseudonyme :</label><br><br>
                     <input type="text" id="PseudonymeConnexion" name="Pseudonyme" required><br>
                     <!-- Correction de l'id pour qu'il soit unique -->
-                    <label for="CodeConnexion">Mot de passe :</label><br>
+                    <label for="CodeConnexion">Mot de passe :</label><br><br>
                     <input type="password" id="CodeConnexion" name="Code" required><br>
                     <!-- Correction du name et rend l'id unique -->
                     <input type="submit" value="Envoyer">
