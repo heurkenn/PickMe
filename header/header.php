810bbe2c4a58e63020764756af68ed5a79dcfef2
nolhan <aurenolhan@cy-tech.fr>
2024-04-11 11:50:49 +0200

    abonnement

--- header/header.php
+++ header/header.php
@@ -3,7 +3,7 @@
         <nav>
             <ul>
                 <li><a href="apropos.php">À propos</a></li>
-                <li><a href="#">Abonnement</a></li>
+                <li><a href="abonnement.php">Abonnement</a></li>
                 <li><a href="contact.php">Contact</a></li>
                 <li><a href="message.php">Tes matchs</a></li>
             </ul>
