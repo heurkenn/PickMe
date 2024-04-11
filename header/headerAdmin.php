753538f211fc2fbc0a3b05189f31c83e53c01366
heurkenn <heurkenn@gmail.com>
2024-04-11 11:52:00 +0200

    pui

---
+++ header/headerAdmin.php
@@ -0,0 +1,10 @@
+<header class="header">
+        <h1><a href="index.php" class="custom-link">PICK ME !</h1>
+        <nav>
+            <ul>
+                <li><a href="report.php">Rapport</a></li>
+                <li><a href="admin.php">Gestion</a></li>
+                
+            </ul>
+        </nav>
+    </header>
