381d25a360549c51b894cd5efd0021413a35a878
nolhan <aurenolhan@cy-tech.fr>
2024-04-12 02:34:22 +0200

    rien

--- footer/footer.php
+++ footer/footer.php
@@ -1,20 +1,19 @@
 <footer class="footer">
-<link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
-<link rel="stylesheet" href="footer/footer.css">
-
+        <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+        <link rel="stylesheet" href="footer/footer.css">
 <div class="icons">
-    <a href="https://www.instagram.com/justice_restaurative/?hl=fr" target="_blank" class="icon icon--instagram">
+    <a href="https://r.mtdv.me/watch?v=yxULlx4xLr" target="_blank" class="icon icon--instagram">
       <i class="ri-instagram-line"></i>
     </a>
-    <a href="https://twitter.com/IFJR_officiel" target="_blank" class="icon icon--twitter">
+    <a href="https://r.mtdv.me/watch?v=yxULlx4xLr" target="_blank" class="icon icon--twitter">
       <i class="ri-twitter-line"></i>
     </a>
-    <a href="https://www.facebook.com/IFJRofficiel/" target="_blank" class="icon icon--facebook">
+    <a href="https://r.mtdv.me/watch?v=yxULlx4xLr" target="_blank" class="icon icon--facebook">
       <i class="ri-facebook-line"></i>
-</a>
+    </a>
    
   </div>
-        <p>&copy; 2024 PICK ME ! Tous droits réservés.</p>
+        <p>&copy; 2024 PICK ME!. Tous droits réservés.</p>
 </footer>
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
-  <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
